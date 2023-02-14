0e9f6c230c718f6894334a2e500b794dfbe33e32
Tomas Aguirrezabala <tomiaguirrezabala@gmail.com>
2023-02-14 19:33:13 -0300

    ..

--- ui app/app.py
+++ ui app/app.py
@@ -5,14 +5,14 @@ from os import system
 # printear = peliculas.json
 # print(printear)
 
-generosData = rq.get('http://127.0.0.1:5000/generos')
-generos = generosData.json()
-for genero in generos:
-    print(genero["generoNombre"])
+# generosData = rq.get('http://127.0.0.1:5000/generos')
+# generos = generosData.json()
+# for genero in generos:
+#     print(genero["generoNombre"])
 
 print("---MENU---")
 print("opcion 1: que generos hay")
-opcionmenu1=int(input("decime 1: "))
+opcionmenu1=int(input("decime 1 o 2: "))
 
 if  opcionmenu1 == 1:
     system("cls")
@@ -20,4 +20,20 @@ if  opcionmenu1 == 1:
     generos = generosData.json()
     for genero in generos:
         print(f'Los generos disponibles son: {genero["generoNombre"]}')
+elif opcionmenu1 == 2:
+    system("cls")
+    pelisData = rq.get("http://127.0.0.1:5000/pelis")
+    peliculas = pelisData.json()
+    print("Las peliculas disponibles son: ")
+    for pelicula in peliculas:
+        print(pelicula["nombre"])
+elif opcionmenu1 == 3:
+    system("cls")
+    directoresData = rq.get("http://127.0.0.1:5000/directores")
+    directores = directoresData.json()
+    print("Los directores disponibles son: ")
+    for director in directores:
+        print(directores)       
+        
+
             
